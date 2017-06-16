8d1896cea9002456ab48f46beceee51d6a4088f9
GiselaFont <giselaaa17@hotmail.com>
2017-06-16 12:44:24 -0500

    first commit

---
+++ WebProject/WebProject/Data/Tables.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProject.Data
+{
+    public class Tables
+    {
+        [Key]
+        public int Id { get; set; }
+        public String cell11 { get; set; }
+
+        public String cell12 { get; set; }
+
+        public String cell13 { get; set; }
+
+        public String cell21 { get; set; }
+
+        public String cell22 { get; set; }
+
+        public String cell23 { get; set; }
+
+        public String cell31 { get; set; }
+
+        public String cell32 { get; set; }
+
+        public String cell33 { get; set; }
+
+        public Tables()
+        {
+
+        }
+    }
+}
---
+++ WebProject/WebProject/Models/TicTacToeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProject.Data;
+
+namespace WebProject.Models
+{
+    public class TicTacToeViewModel
+    {
+        public List<Tables> Table { get; set; } = new List<Tables>();
+    }
+}
